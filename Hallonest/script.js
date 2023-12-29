3b6c15797e0385b3bce1c9db6d3c321490e66dbc
Bol2riz02 <maxime.allasio@etu.univ-amu.fr>
2023-12-29 19:04:38 +0100

    test

--- Hallonest/script.js
+++ Hallonest/script.js
@@ -1,24 +1,31 @@
 function noir() {
   var element = document.body;
-  element.classList.toggle("dark-mode");
+  element.classList.remove("light-mode", "pink-mode", "blue-mode", "green-mode", "yellow-mode");
+  element.classList.add("dark-mode");
 }
 function blanc() {
   var element = document.body;
-  element.classList.toggle("light-mode");
+  element.classList.remove("dark-mode", "pink-mode", "blue-mode", "green-mode", "yellow-mode");
+  element.classList.add("light-mode");
+
 }
 function rose() {
   var element = document.body;
-  element.classList.toggle("pink-mode");
+  element.classList.remove("light-mode", "dark-mode", "blue-mode", "green-mode", "yellow-mode");
+  element.classList.add("pink-mode");
 }
 function bleu() {
   var element = document.body;
-  element.classList.toggle("blue-mode");
+  element.classList.remove("light-mode", "pink-mode", "dark-mode", "green-mode", "yellow-mode");
+  element.classList.add("blue-mode");
 }
 function vert() {
   var element = document.body;
-  element.classList.toggle("green-mode");
+  element.classList.remove("light-mode", "pink-mode", "blue-mode", "dark-mode", "yellow-mode");
+  element.classList.add("green-mode");
 }
 function jaune() {
   var element = document.body;
-  element.classList.toggle("yellow-mode");
+  element.classList.remove("light-mode", "pink-mode", "blue-mode", "green-mode", "dark-mode");
+  element.classList.add("yellow-mode");
 }
