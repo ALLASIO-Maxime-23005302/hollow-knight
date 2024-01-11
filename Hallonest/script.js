9ef50c8ceb8af93b19a1fab8e17559c0fc5214f5
Bol2riz02 <maxime.allasio@etu.univ-amu.fr>
2024-01-11 18:26:44 +0100

    Test

--- Hallonest/script.js
+++ Hallonest/script.js
@@ -1,31 +1,15 @@
-function noir() {
-  var element = document.body;
-  element.classList.remove("light-mode", "pink-mode", "blue-mode", "green-mode", "yellow-mode");
-  element.classList.add("dark-mode");
-}
-function blanc() {
-  var element = document.body;
-  element.classList.remove("dark-mode", "pink-mode", "blue-mode", "green-mode", "yellow-mode");
-  element.classList.add("light-mode");
+//Couleur du fond
+let themeSelector = document.querySelector('#theme');
+themeSelector.addEventListener('change', function() {
+  let body = document.querySelector('body');
+  body.classList.remove("light-mode", "pink-mode", "blue-mode", "green-mode", "yellow-mode", "dark-mode");
 
-}
-function rose() {
-  var element = document.body;
-  element.classList.remove("light-mode", "dark-mode", "blue-mode", "green-mode", "yellow-mode");
-  element.classList.add("pink-mode");
-}
-function bleu() {
-  var element = document.body;
-  element.classList.remove("light-mode", "pink-mode", "dark-mode", "green-mode", "yellow-mode");
-  element.classList.add("blue-mode");
-}
-function vert() {
-  var element = document.body;
-  element.classList.remove("light-mode", "pink-mode", "blue-mode", "dark-mode", "yellow-mode");
-  element.classList.add("green-mode");
-}
-function jaune() {
-  var element = document.body;
-  element.classList.remove("light-mode", "pink-mode", "blue-mode", "green-mode", "dark-mode");
-  element.classList.add("yellow-mode");
-}
+  body.classList.add(`${this.value}-mode`);
+});
+
+
+// Navbar
+window.addEventListener("scroll", function() {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
